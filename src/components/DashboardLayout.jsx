cb98873643ee393f0570b32ddf9a79046babf660
Azizi Faris <farisazizishie@gmail.com>
2026-03-16 05:26:24 +0700

    optimization user interface

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -37,19 +37,19 @@ const DashboardLayout = () => {
             )
         },
         {
-            name: 'Reservasi Baru',
-            path: '/registration',
-            roles: ['Superadmin', 'Admin', 'Resepsionis'],
-            icon: (
-                <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
-            )
-        },
-        {
             name: 'User Management',
             path: '/users',
             roles: ['Superadmin'],
             icon: (
                 <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+            )
+        },
+        {
+            name: 'Reservasi Baru',
+            path: '/registration',
+            roles: ['Superadmin', 'Admin', 'Resepsionis'],
+            icon: (
+                <svg className="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
             )
         },
     ];
